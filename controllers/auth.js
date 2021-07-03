e700f23f2f852e8af2445b90c7f33c1cc8e93416
Kyle Friel <businessfriel@gmail.com>
2021-07-03 11:32:08 -0400

    JWT setup.

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,7 @@ exports.register = async (req, res, next) => {
             email,
             password,
         });
-        res.status(201).json({
-            success: true,
-            user,
-        });
+        sendToken(user, 201, res);
     } catch (error) {
         next(error);
     }
@@ -36,11 +33,7 @@ exports.login = async (req, res, next) => {
         if (!isMatch) {
             return next(new ErrorResponse("Invalid credentials", 401));
         }
-
-        res.status(200).json({
-            success: true,
-            token: "tsdfks224",
-        });
+        sendToken(user, 200, res);
     } catch (error) {
         next(error);
     }
@@ -50,4 +43,9 @@ exports.forgotpassword = (req, res, next) => {
 };
 exports.resetpassword = (req, res, next) => {
     res.send("Reset Password Route");
+};
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken();
+    res.status(statusCode).json({ success: true, token });
 };

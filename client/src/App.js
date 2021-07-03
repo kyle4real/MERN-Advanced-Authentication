c582d5c15926ae1e632e8c2426988966759108c9
Kyle Friel <businessfriel@gmail.com>
2021-07-03 13:41:43 -0400

    Added all routing, screens, and corresponding scss files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Routing
+import { PrivateRoute } from "./components/routing/PrivateRoute";
+
+// Screens
+import PrivateScreen from "./components/routing/PrivateScreen";
+import LoginScreen from "./components/routing/LoginScreen";
+import RegisterScreen from "./components/routing/RegisterScreen";
+import ForgotPasswordScreen from "./components/routing/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/routing/ResetPasswordScreen";
+
 const App = () => {
-    return <div className="App">React App</div>;
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <PrivateRoute exact path="/" component={privateScreen} />
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route exact path="/register" component={RegisterScreen} />
+                    <Route exact path="/forgotpassword" component={ForgotPasswordScreen} />
+                    <Route
+                        exact
+                        path="/passwordreset/:resetToken"
+                        component={ResetPasswordScreen}
+                    />
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;

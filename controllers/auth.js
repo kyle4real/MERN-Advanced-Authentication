a2e5da3536e6b5fda4878f77cb9b7f98e2f70771
Kyle Friel <businessfriel@gmail.com>
2021-07-02 22:21:38 -0400

    Login auth. Error handling.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -14,27 +15,35 @@ exports.register = async (req, res, next) => {
             user,
         });
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+        next(error);
     }
 };
 exports.login = async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        res.status(400).json({ success: false, error: "Please provide email and password" });
+        return next(new ErrorResponse("Please provide an email and password", 400));
     }
     try {
         const user = await User.findOne({ email }).select("+password");
 
         if (!user) {
-            res.status(404).json({ success: false, error: "Invalid credentials" });
+            return next(new ErrorResponse("Invalid credentials", 401));
         }
 
-        // const is
-    } catch (error) {}
+        const isMatch = await user.matchPasswords(password);
+
+        if (!isMatch) {
+            return next(new ErrorResponse("Invalid credentials", 401));
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "tsdfks224",
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 exports.forgotpassword = (req, res, next) => {
     res.send("Forgot Password Route");

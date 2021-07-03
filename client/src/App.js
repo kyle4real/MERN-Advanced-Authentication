5cee1bfe78eca6571a6738c8deb5a8120d47f6d4
Kyle Friel <businessfriel@gmail.com>
2021-07-03 13:44:18 -0400

    Added scss for all screens.

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         <Router>
             <div className="app">
                 <Switch>
-                    <PrivateRoute exact path="/" component={privateScreen} />
+                    <PrivateRoute exact path="/" component={PrivateScreen} />
                     <Route exact path="/login" component={LoginScreen} />
                     <Route exact path="/register" component={RegisterScreen} />
                     <Route exact path="/forgotpassword" component={ForgotPasswordScreen} />
